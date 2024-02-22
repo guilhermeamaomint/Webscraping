f1371fec07b3fef370f6344f01c3eb21370f46a9
guilhermegit <g360guilherme@gmail.com>
2024-02-22 04:18:15 -0300

    Criação do repisitorio git

---
+++ futebola/pythonProject/ftbol/ftbol/spiders/futbol.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class ImdbSpider(scrapy.Spider):
+    name = "futbol"
+    start_urls = ['https://www.cbf.com.br/futebol-brasileiro?csrt=8376204594349034325']
+
+    def parse(self, response):
+        for timesa in response.css('td:nth-child(1)'):
+            yield{
+                "Classificacao": timesa.css('b.m-l-10::text').get(),
+                " nome do time": timesa.css('span.hidden-xs::text').get()[:-4],
+                " estado do time": timesa.css('span.hidden-xs::text').get()[-2:]
+            }
+
+
+# for nome in nomesdostimes:
+#   print(nome[:-5])    NOME DOS TIMES SEM ESTADO
+#   print(nome[-2:])    ESTADO SEM O NOME DOS TIMES
